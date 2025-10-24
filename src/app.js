45a012a1160b8a4fc469e1798d2ad97c942206ea
Apple <apple@Apples-MacBook-Pro.local>
2025-10-24 18:46:21 +0530

    testing

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true }));
 
 app.get("/",(req,res)=>{
-    res.send("API is working")
+    res.send("API is working hiiii")
 })
 // app.post("/api/stock-items/create",(req,res)=>{
 //     console.log("Stock items endpoint hit",req.body);
