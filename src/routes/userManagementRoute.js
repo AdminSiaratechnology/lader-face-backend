dc85f3c7ca9f3609290babd9410329fc899d08dc
SnehaSiara <sneha@siaratechnology.com>
2025-10-30 11:09:08 +0530

    users

--- src/routes/userManagementRoute.js
+++ src/routes/userManagementRoute.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userManagement = require('../controllers/userManagementControler');
 
-router.get('/client/allUsers', userManagement.getAllClientUsersWithCompany);
-router.get('/client/:companyId', userManagement.getAllClientUsersWithCompany);
+router.get('/client/allUsers', userManagement.getAllClientUsers);
+// router.get('/client/:companyId', userManagement.getAllClientUsersWithCompany);
 
 
 module.exports = router;
